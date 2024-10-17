c7249cfbbbf309b173e8b766e97ef32cce0004d2
Luka <luka.luka.luka93@gmail.com>
2024-10-17 14:02:37 -0700

    made a custom render, made a set for the valid tags, made a function that will turn tags into lowercase and check if its there, if yes it will return it, if not it will return empty string, then we implemented it into a render

--- client/src/components/IdeaList.js
+++ client/src/components/IdeaList.js
@@ -17,6 +17,25 @@ class IdeaList {
                 date: '02/01/2023'
             },
         ];
+            this._validTags = new Set();
+            this._validTags.add('technology');
+            this._validTags.add('software');
+            this._validTags.add('business');
+            this._validTags.add('education');
+            this._validTags.add('health');
+            this._validTags.add('inventions');
+    };
+// new Set() - is a data structure, it's basically a list of unique values
+
+    getTagClass(tag) {
+        tag = tag.toLowerCase();
+        let tagClass = '';
+        if ( this._validTags.has(tag)) {
+            tagClass = `tag-${tag}`;
+        } else {
+            tagClass = '';
+        }
+        return tagClass;
     }
 /* Render, ideaListEl innerHTML will loop through with map, and will say:
 for each idea will return a card in a template string , and .join('') which will turn it
@@ -24,17 +43,17 @@ back into a string
 */
     render() {
         this._ideaListEl.innerHTML = this._ideas.map( idea => {
+        const tagClass = this.getTagClass(idea.tag);
             return `
         <div class="card">
           <button class="delete"><i class="fas fa-times"></i></button>
           <h3>
-            Positive NewsLetter, a newsletter that only shares positive,
-            uplifting news
+            ${idea.text}
           </h3>
-          <p class="tag tag-technology">TECHNOLOGY</p>
+          <p class="tag ${tagClass}">${idea.tag.toUpperCase()}</p>
           <p>
-            Posted on <span class="date">January 1, 2022</span> by
-            <span class="author">Tony Stark</span>
+            Posted on <span class="date">${idea.date}</span> by
+            <span class="author">${idea.username}</span>
           </p>
         </div>`
         }).join('')
