d9c0bceb2932b26f96ce25c53dee54addd731942
Luka <luka.luka.luka93@gmail.com>
2024-10-17 12:39:36 -0700

    made a render function, add event listeners function, handleSubmit function

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,9 @@
 import '@fortawesome/fontawesome-free/css/all.css';
 import Modal from './components/Modal';
+import IdeaForm from './components/IdeaForm';
 import './css/style.css';
 
 const modal = new Modal();
-
-
-
-
-
+const ideaForm = new IdeaForm();
+ideaForm.render();
+//I have to call on ideaForm.render() to render the modal intake form function
