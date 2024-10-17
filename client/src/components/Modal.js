681b71690de0127a0a2af92cd4af375e8cd89378
Luka <luka.luka.luka93@gmail.com>
2024-10-17 12:26:19 -0700

    started with front end, made a folder named components, and made Modal.js class file where I made class with functions to open and close the modal

---
+++ client/src/components/Modal.js
@@ -0,0 +1,36 @@
+class Modal {
+    constructor () {
+        this._modal = document.querySelector('#modal')
+        this._modalBtn = document.querySelector('#modal-btn')
+/*Now this addEventListeners arent being called anywhere, so we are gonna call them right in 
+the constructor
+*/
+        this.addEventListeners();
+    }
+
+    addEventListeners(){
+        this._modalBtn.addEventListener('click', this.open.bind(this));
+        window.addEventListener('click', this.outsideClick.bind(this));
+    }
+/* Remember, when you use the .this keyword inside of a class method, and its on an 
+event listener .this (.this._modal.style.display) is going to pertain to the element that 
+the event was called on. What we need to do is use .bind(this). Which says:
+To open() this now pertains to the class rather than the element
+
+
+*/
+    open () {
+    this._modal.style.display = 'block';
+    }
+
+    close() {
+    this._modal.style.display = 'none';
+    }
+    outsideClick(e) {
+    if(e.target === this._modal) {
+        close();
+    }
+    }
+}
+
+export default Modal;
--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,10 @@
 import '@fortawesome/fontawesome-free/css/all.css';
+import Modal from './components/Modal';
 import './css/style.css';
+
+const modal = new Modal();
+
+
+
+
 
